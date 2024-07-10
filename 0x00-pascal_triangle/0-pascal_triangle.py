848e4f540065962cc960cc5cba355453086b3de8
Thabang Maphangisane <thabangh647@gmail.com>
2024-07-10 20:34:28 +0200

    0. Pascal's Triangle - pycodestyle errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-"""This script has a function that returns a list of lists of integers representing the Pascal’s triangle of n"""
+"""
+This script has a function that returns a list of lists of integers
+representing the Pascal’s triangle of n
+"""
+
 
 def pascal_triangle(n):
+    # Initialize the Triangle
     triangle = []
-    
-    # returns empty list
+
+    # Check for Non-Positive Input & return empty list if true
     if n <= 0:
         return triangle
-    
-    # Outer loop (rows of the triangle)
+
+    # Outer loop (generate each row of the triangle)
     for i in range(n):
         # initilize to 0 after each iteration
         current_row = []
-        
-        # inner loop (each element in current row)
+
+        # inner loop (generate each element in current row)
         for j in range(i + 1):
-            # checks if j is 0 or i is first or last element of the row and add 1)
+            # checks to add [1] to first or last element of the row
             if j == 0 or j == i:
                 current_row.append(1)
             else:
+                # adds previous row elements using indices
                 current_row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        
-        # add current row to the triangle
+
+        # adds current row to the triangle
         triangle.append(current_row)
-    
+
     return triangle

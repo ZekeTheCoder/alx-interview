8f6aee4ce2de00858db926d109c61accbe011f9a
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-09 01:34:16 +0200

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,27 @@
 #!/usr/bin/node
-// Script that prints all characters of a Star Wars movie
- 
+/* Script that prints all characters of a Star Wars movie */
+const request = require('request');
 const filmsApiUrl = 'https://swapi-api.hbtn.io/api/films/';
 const movieId = process.argv[2];
 
+// Function to fetch data from a given URL and return a Promise 
 function fetchData(url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (error) {
-        reject(error); // Reject the Promise if there is an error
+        reject(error);
       } else {
-        resolve(JSON.parse(body)); // Resolve the Promise with parsed JSON data
+        resolve(JSON.parse(body));
       }
     });
   });
 }
 
+// function to fetch and print Star Wars characters from a specified movie
 async function printStarWarsCharacters() {
   try {
     const filmData = await fetchData(`${filmsApiUrl}${movieId}/`);
-    const characterUrls = filmData.characters; // Get the list of character URLs
+    const characterUrls = filmData.characters;
 
     for (const characterUrl of characterUrls) {
       try {

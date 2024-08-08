aec8c65921ad2f7f320604388e78a28a54aac620
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-09 01:40:46 +0200

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,40 +1,41 @@
 #!/usr/bin/node
 /* Script that prints all characters of a Star Wars movie */
-const request = require('request');
-const filmsApiUrl = 'https://swapi-api.hbtn.io/api/films/';
-const movieId = process.argv[2];
 
-// Function to fetch data from a given URL and return a Promise 
-function fetchData(url) {
+const request = require('request')
+const filmsApiUrl = 'https://swapi-api.hbtn.io/api/films/'
+const movieId = process.argv[2]
+
+// Function to fetch data from a given URL and return a Promise
+function fetchData (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (error) {
-        reject(error);
+        reject(error)
       } else {
-        resolve(JSON.parse(body));
+        resolve(JSON.parse(body))
       }
-    });
-  });
+    })
+  })
 }
 
-// function to fetch and print Star Wars characters from a specified movie
-async function printStarWarsCharacters() {
+// Function to fetch and print Star Wars characters from a specified movie
+async function printStarWarsCharacters () {
   try {
-    const filmData = await fetchData(`${filmsApiUrl}${movieId}/`);
-    const characterUrls = filmData.characters;
+    const filmData = await fetchData(`${filmsApiUrl}${movieId}/`)
+    const characterUrls = filmData.characters
 
     for (const characterUrl of characterUrls) {
       try {
-        const characterData = await fetchData(characterUrl);
-        console.log(characterData.name);
+        const characterData = await fetchData(characterUrl)
+        console.log(characterData.name)
       } catch (error) {
-        console.error(`Error fetching character data: ${error.message}`);
+        console.error(`Error fetching character data: ${error.message}`)
       }
     }
   } catch (error) {
-    console.error(`Error fetching film data: ${error.message}`);
-    process.exit(1); // Exit with a non-zero status code on error
+    console.error(`Error fetching film data: ${error.message}`)
+    process.exit(1) // Exit with a non-zero status code on error
   }
 }
 
-printStarWarsCharacters();
+printStarWarsCharacters()
